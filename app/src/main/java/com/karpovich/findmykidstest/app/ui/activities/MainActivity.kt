bccfa53228da22b524d5efb2f73dd15fdc241a67
alex <natalia42010822@mail.ru>
2024-02-10 13:37:25 +0300

    Implemented View Binding in MainActivity

--- app/src/main/java/com/karpovich/findmykidstest/app/ui/activities/MainActivity.kt
+++ app/src/main/java/com/karpovich/findmykidstest/app/ui/activities/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.karpovich.findmykidstest.R
+import com.karpovich.findmykidstest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
