8a328f7473683df4d3be2a3551cc4f45334803a7
alex <natalia42010822@mail.ru>
2024-02-10 11:17:59 +0300

    Add Splash Screen Theme, Color, and Fonts: Introduce splash.xml with Theme.SplashScreen customization, including a serene_sky_blue color (#E3EBF8) and the addition of roboto_medium.ttf and roboto_regular.ttf fonts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.karpovich.findmykidstest"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.karpovich.findmykidstest"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/karpovich/findmykidstest/app/ui/activities/MainActivity.kt
+++ app/src/main/java/com/karpovich/findmykidstest/app/ui/activities/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.karpovich.findmykidstest
+package com.karpovich.findmykidstest.app.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.karpovich.findmykidstest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
