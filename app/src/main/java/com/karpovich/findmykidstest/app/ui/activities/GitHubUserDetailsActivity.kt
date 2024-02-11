489a7ea0a359afcc053331e5e1b0b36a96610978
alex <natalia42010822@mail.ru>
2024-02-11 13:34:45 +0300

    added a method that parses the intent

--- app/src/main/java/com/karpovich/findmykidstest/app/ui/activities/GitHubUserDetailsActivity.kt
+++ app/src/main/java/com/karpovich/findmykidstest/app/ui/activities/GitHubUserDetailsActivity.kt
@@ -4,16 +4,26 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.karpovich.findmykidstest.R
+import com.karpovich.findmykidstest.app.data.AppRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class GitHubUserDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_git_hub_user_details)
+        parseIntent(intent)
+    }
+    private fun parseIntent(intent: Intent) {
+        val login = intent.getStringExtra(EXTRA_LOGIN)
+        Log.d("инт", "parseIntent: $login")
     }
     companion object {
-        private const val EXTRA_LOGIN = "extra_login"
 
+        private const val EXTRA_LOGIN = "extra_login"
         fun newIntent(context: Context, login: String): Intent {
             val intent = Intent(context, GitHubUserDetailsActivity::class.java)
             intent.putExtra(EXTRA_LOGIN, login)
