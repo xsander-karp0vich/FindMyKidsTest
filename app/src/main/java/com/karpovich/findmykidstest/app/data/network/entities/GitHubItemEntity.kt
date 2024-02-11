9cee27f07194305cab7ff06d5a80ba34c48151f7
alex <natalia42010822@mail.ru>
2024-02-11 12:30:06 +0300

    created data models

--- app/src/main/java/com/karpovich/findmykidstest/app/data/network/entities/GitHubItemEntity.kt
+++ app/src/main/java/com/karpovich/findmykidstest/app/data/network/entities/GitHubItemEntity.kt
@@ -2,7 +2,7 @@ package com.karpovich.findmykidstest.app.data.network.entities
 
 import com.google.gson.annotations.SerializedName
 
-data class GitHubUserEntity(
+data class GitHubItemEntity(
     val login: String,
     val id: Int,
     @SerializedName("node_id") val nodeId: String,
@@ -19,21 +19,6 @@ data class GitHubUserEntity(
     @SerializedName("repos_url") val reposUrl: String,
     @SerializedName("events_url") val eventsUrl: String,
     @SerializedName("received_events_url") val receivedEventsUrl: String,
-    @SerializedName("type") val type: String,
-    @SerializedName("site_admin") val siteAdmin: Boolean,
-    @SerializedName("name") val name: String?,
-    @SerializedName("company") val company: String?,
-    @SerializedName("blog") val blog: String?,
-    @SerializedName("location") val location: String?,
-    @SerializedName("email") val email: String?,
-    @SerializedName("hireable") val hireable: Boolean?,
-    @SerializedName("bio") val bio: String?,
-    @SerializedName("twitter_username") val twitterUsername: String?,
-    @SerializedName("public_repos") val publicRepos: Int,
-    @SerializedName("public_gists") val publicGists: Int,
-    @SerializedName("followers") val followers: Int,
-    @SerializedName("following") val following: Int,
-    @SerializedName("created_at") val createdAt: String,
-    @SerializedName("updated_at") val updatedAt: String
+    val type: String,
+    @SerializedName("site_admin") val siteAdmin: Boolean
 )
-
