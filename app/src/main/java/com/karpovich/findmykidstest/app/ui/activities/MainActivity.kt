7133ab51f74623df049f7604fac03bbdef2a53c5
alex <natalia42010822@mail.ru>
2024-02-12 11:28:05 +0300

    renamed variables to 'MainActivity' and deleted unnecessary code

--- app/src/main/java/com/karpovich/findmykidstest/app/ui/activities/MainActivity.kt
+++ app/src/main/java/com/karpovich/findmykidstest/app/ui/activities/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.karpovich.findmykidstest.app.ui.activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import com.karpovich.findmykidstest.app.ui.adapters.GitHubItemsAdapter
+import com.karpovich.findmykidstest.app.ui.adapters.GitHubUserAdapter
 import com.karpovich.findmykidstest.app.ui.viewmodels.MainViewModel
 import com.karpovich.findmykidstest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
-    private lateinit var gitHubItemsAdapter: GitHubItemsAdapter
+    private lateinit var gitHubUserAdapter: GitHubUserAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
@@ -34,17 +33,16 @@ class MainActivity : AppCompatActivity() {
             setupProgressBar(it)
         }
         viewModel.gitHubUsers.observe(this){
-            Log.d("таг", "observeViewModel: $it")
             if (it != null) {
-                gitHubItemsAdapter.submitList(it)
+                gitHubUserAdapter.submitList(it)
             }
         }
     }
     private fun setupRecycleView() {
         val rvGitHubUsers = binding.usersRecycleView
         with(rvGitHubUsers){
-            gitHubItemsAdapter = GitHubItemsAdapter()
-            adapter = gitHubItemsAdapter
+            gitHubUserAdapter = GitHubUserAdapter()
+            adapter = gitHubUserAdapter
             val spanCount = 2
             val layoutManager = GridLayoutManager(this@MainActivity, spanCount)
             rvGitHubUsers.layoutManager = layoutManager
@@ -53,18 +51,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun setupClickListener() {
-        gitHubItemsAdapter.onGitUserClickListener = {
-            Log.d("таг", "setupClickListener: ${it.login}")
-            launchToGitHubUserDetailsActivity(it.login)
+        gitHubUserAdapter.onGitUserClickListener = {
+            it.login?.let { it1 -> launchToGitHubUserDetailsActivity(it1) }
         }
     }
     private fun setupOnEndOfListReachedListener() {
-        gitHubItemsAdapter.onEndOfListReachedListener = {
+        gitHubUserAdapter.onEndOfListReachedListener = {
             //вспомнил что пагинация не нужна и обрадовался
         }
     }
     private fun launchToGitHubUserDetailsActivity(login:String) {
-        val intent = GitHubUserDetailsActivity.newIntent(this, login)
+        val intent = UserDetailsActivity.newIntent(this, login)
         startActivity(intent)
     }
     private fun setupProgressBar(boolean: Boolean) {
